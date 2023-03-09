e6a99c0c29364fd1d8e257980a8e3f2943a16f21
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-03-09 22:27:29 +0530

    chime-perf: import perf from lemon V14.0.2.0.SJQCNXM
     - from lemon-user 12 SKQ1.211202.001 V14.0.2.0.SJQCNXM release-keys
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- BoardConfigPerf.mk
+++ BoardConfigPerf.mk
@@ -6,4 +6,7 @@
 
 PERF_PATH := vendor/xiaomi/chime-perf
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(PERF_PATH)/manifests/perf_framework_matrix.xml
+DEVICE_MANIFEST_FILE += $(PERF_PATH)/manifests/perf_manifest.xml
+
 include vendor/xiaomi/chime-perf/common/BoardConfigVendor.mk
--- perf.mk
+++ perf.mk
@@ -8,4 +8,33 @@
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Boot Jars
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
+# Configs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf/,$(TARGET_COPY_OUT_VENDOR)/etc/perf) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/lm/,$(TARGET_COPY_OUT_VENDOR)/etc/lm) \
+
+# Packages
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+    libpsi.vendor \
+    libtflite \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
+# Properties
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.perf-hal.ver=2.2 \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.perf.scroll_opt=true \
+    ro.vendor.qspm.enable=true \
+    vendor.pasr.activemode.enabled=true \
+    vendor.power.pasr.enabled=true
+
 $(call inherit-product, vendor/xiaomi/chime-perf/common/common-vendor.mk)
